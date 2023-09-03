bff5e7c9d5de6950a5ac829e71d27980855068b2
SaDyZ P120 <ponlamart_t@silpakorn.edu>
2023-09-03 12:11:15 +0700

    sending1

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -54,11 +54,175 @@ class _HomePageState extends State<HomePage> {
 
   _buildQuizView() {
     // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25.0),
+        border: Border.all(
+          width: 4.0,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 0), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(14.0),
+              margin: const EdgeInsets.all(20.0),
+              child: Text('Question 1 of 30',style: TextStyle(fontSize: 30.0),),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color:Colors.greenAccent,
+                borderRadius: BorderRadius.circular(25.0),
+                border: Border.all(
+                  width: 2.0,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text("Which planet is known as the 'Red Planet'",style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold,color: Colors.white),),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 16, 10, 10),
+              child: choicebuttom(
+                'A',
+                'Mars',
+                true,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 16, 10, 10),
+              child: choicebuttom(
+                'B',
+                'Urenus',
+                false,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 16, 10, 10),
+              child: choicebuttom(
+                'C',
+                'Saturn',
+                false,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 16, 10, 10),
+              child: choicebuttom(
+                'D',
+                'Earth',
+                false,
+              ),
+            ),
+
+          ],
+    ),
+      ),
+    );
+  }
+
+  Container choicebuttom(
+  String text,
+  String textdata,
+  bool isCorrected,
+      ) {
+    return Container(
+            decoration: BoxDecoration(
+              color: isCorrected ? Color.fromARGB(255, 204, 255, 255) : Colors.white70,
+              borderRadius: BorderRadius.circular(25.0),
+              border: Border.all(
+                width: 2.0,
+              ),
+            ),
+            child: Row(
+                //mainAxisAlignment: MainAxisAlignment.,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: isCorrected ? Color.fromARGB(255, 255, 102, 102) : Colors.white70,
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                          width: 2.0,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text(text,style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold ,color: Colors.black),),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Row(
+                      children: [
+                        Text(textdata,style: TextStyle(fontSize: 20.0,color: Colors.black,fontWeight: FontWeight.bold),),
+                        Container(
+                          padding: const EdgeInsets.all(8.0),
+                          margin: const EdgeInsets.all(12.0),
+                          decoration: BoxDecoration(
+                            color: isCorrected ? Color.fromARGB(255, 0, 255, 128) : Colors.white70 ,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.done,
+                            color: isCorrected ? Colors.black : Colors.white70,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                ]
+            ),
+          );
   }
 
   _buildButtonPanel() {
     // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 12.0,bottom: 12.0),
+              child: Icon(Icons.arrow_back_ios,color: Colors.black,size: 30,),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.redAccent,
+              shadowColor: Colors.lightBlue,
+            ),
+            onPressed: () {},
+          ),
+        ),
+        Expanded(
+          child: ElevatedButton(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 12.0,bottom: 12.0),
+              child: Icon(Icons.arrow_forward_ios,color: Colors.black,size: 30,),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.greenAccent,
+              shadowColor: Colors.lightBlue,
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ],
+    );
   }
 }
